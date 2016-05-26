1b0b6967797c3ee53c41743d6dc9ad25ea8ac901
Ionică Bizău <bizauionica@gmail.com>
2016-05-26 20:01:47 +0300

    Improve the cli functionality

--- bin/flight-tracker.js
+++ bin/flight-tracker.js
@@ -1,11 +1,50 @@
 #!/usr/bin/env node
 
 const flightTracker = require("..")
+    , Tilda = require("tilda")
     ;
 
-flightTracker({
-    start: [process.argv[4] || "Start", new Date(process.argv[2])]
-  , end: [process.argv[5] || "End.", new Date(process.argv[3])]
-  , decimals: 5
-  , interval: 100
+new Tilda(`${__dirname}/../package.json`, {
+    args: [
+        {
+            name: "startDate"
+          , desc: "The start date."
+        }
+      , {
+            name: "endDate"
+          , desc: "The end date."
+        }
+      , {
+            name: "start"
+          , desc: "The start location."
+        }
+      , {
+            name: "end"
+          , desc: "The end location."
+        }
+    ],
+    options: [
+        {
+            name: "ms"
+          , opts: ["i", "interval"]
+          , type: Number
+          , desc: "The interval value in milliseconds."
+        }
+      , {
+            name: "count"
+          , opts: ["d", "decimals"]
+          , type: Number
+          , desc: "How many decimals to display."
+        }
+    ]
+}).main(a => {
+    flightTracker({
+        start: [a.args.start, new Date(a.args.startDate)]
+      , end: [a.args.end, new Date(a.args.endDate)]
+      , decimals: a.options.decimals.value
+      , interval: a.options.interval.value
+    }).on("error", err => {
+        console.error(err);
+        process.exit(1);
+    });
 });
